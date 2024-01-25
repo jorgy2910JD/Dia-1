8478861f6d103db03d2a4f2b6c66c1f48891f597
Jorge Mozo <jorgy29081@gmail.com>
2024-01-25 09:24:50 -0500

    Python

---
+++ main.py
@@ -0,0 +1,49 @@
+##---------------------------------------
+##---EJercicio 1---
+##---------------------------------------
+
+#Impresion de consola 
+print ( " hola mundo ")
+
+#-----Datos primitivos-----
+#1. String
+Texto =  " CAMPUS " 
+print ( Texto)
+print (type(Texto) )
+#2. Int
+numeroentero = 1 
+print (numeroentero)
+
+#3.Double
+numerodecimallargo =3.1416239823982
+print (numerodecimallargo)
+#4. Float
+numerodecimal= 3.5
+print(numerodecimal)
+#5 Boolean 
+booleano= True
+print(booleano)
+#------Entrada por parte del usuario------
+entradausuario = input( "ingresa tu nombre: ")
+print ("tu nombre es:" , entradausuario)
+#----------Entrada por parte del usuario con definicion de datos primitivos--------
+entradausuarioNumero =input ("Ingresa tu edad: ")
+numeroFinal = int (entradausuarioNumero)
+print ("Tu edad es: " , numeroFinal)
+
+#------Ciclos------
+#ciclo for 
+for i in range (5,10): # for contador in range (desde, hasta, pasos)
+    print (i)
+#ciclo while
+    booleanito = True 
+    while booleanito == True: #while condición_a_cumplir :
+        print ("sigo vivo")
+        booleanito= False
+        #-------condicionales--------
+     texto1 = "Campus"
+    if texto1 == "Campus":
+        Print ("Soy campus")
+    else:
+        Print ("No soy campus")
+##Desarrollado por JORGE LUIS MOZO CASTRO - 1099738869
