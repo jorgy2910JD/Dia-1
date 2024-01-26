1258f222ee50e412376d928783fa35d27b279eb0
Jorge Mozo <jorgy29081@gmail.com>
2024-01-26 07:19:48 -0500

    Dia 1

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
+
 ##---------------------------------------
 ##---EJercicio 1---
 ##---------------------------------------
 
+#----------Funcion Sin/Sin---------------
+def saludo():
+    print("hola")
 #Impresion de consola 
+Fun1 = saludo()
 print ( " hola mundo ")
 
+#----------Funcion Con/Con---------------
+def Operacion(a,b):
+    c = a + b
+    return c
+n = int(input("Digite un número"))
+t = int(input("Digite otro número"))
+print(Operacion(n,t))
+
+#---------Función Con/Sin----------------
+def suma(a,b):
+    c = a + b 
+    print(c)
+a = 5
+b = 10
+Fun2 = suma(a,b)
+
+#---------Función Sin/Con----------------
+def multiplicacion():
+    print ("multiplicaión de dos numeros")
+    Num1= float(input("ingrese el primer número por favor"))
+    Num2= float(input("ingrese el segundo número por favor"))
+    return Num1*Num2
+result= multiplicacion()
+print(result)
+
+ 
 #-----Datos primitivos-----
 #1. String
 Texto =  " CAMPUS " 
@@ -24,11 +55,11 @@ print(numerodecimal)
 booleano= True
 print(booleano)
 #------Entrada por parte del usuario------
-entradausuario = input( "ingresa tu nombre: ")
-print ("tu nombre es:" , entradausuario)
+User = input( "ingresa tu nombre: ")
+print ("tu nombre es:" , User)
 #----------Entrada por parte del usuario con definicion de datos primitivos--------
-entradausuarioNumero =input ("Ingresa tu edad: ")
-numeroFinal = int (entradausuarioNumero)
+Edad =input ("Ingresa tu edad: ")
+numeroFinal = int (Edad)
 print ("Tu edad es: " , numeroFinal)
 
 #------Ciclos------
@@ -41,9 +72,9 @@ for i in range (5,10): # for contador in range (desde, hasta, pasos)
         print ("sigo vivo")
         booleanito= False
         #-------condicionales--------
-     texto1 = "Campus"
+    texto1 = "Campus"
     if texto1 == "Campus":
-        Print ("Soy campus")
+        print ("Soy campus")
     else:
-        Print ("No soy campus")
+        print ("No soy campus")
 ##Desarrollado por JORGE LUIS MOZO CASTRO - 1099738869
